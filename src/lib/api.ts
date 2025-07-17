48655b332a438abbad0ad0664e10fa1191360a0c
MarinaKorn <gecagem840@gmail.com>
2025-07-17 23:20:53 +0200

    6

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import type { Note, NoteForPost } from '../../types/note';
 
-
-axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
+axios.defaults.baseURL = 'https://notehub-public.goit.study/api';
 
 const AUTH_HEADER = {
   Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
 };
+
 export type NotesResponse = {
   notes: Note[];
   totalPages: number;
