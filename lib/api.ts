7b9a85fd4f71b772eab1fe58eb9170d57f4d23cc
MarinaKorn <gecagem840@gmail.com>
2025-07-18 21:51:17 +0200

    12

--- lib/api.ts
+++ lib/api.ts
@@ -4,7 +4,7 @@ import type { Note, NoteForPost } from '../types/note';
 axios.defaults.baseURL = 'https://notehub-public.goit.study/api';
 
 const AUTH_HEADER = {
-  Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
+  Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`
 };
 
 export type NotesResponse = {
